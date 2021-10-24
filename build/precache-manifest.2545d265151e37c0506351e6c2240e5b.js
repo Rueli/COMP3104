5e6c377e8706d7c06c8e32b2b81a652796a6dd21
Rueli <Alsklingen2308>
2021-10-23 22:48:11 -0400

    chnaged master to main

---
+++ build/precache-manifest.2545d265151e37c0506351e6c2240e5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae4989c3046564355f18",
+    "url": "/Rueli/COMP3104/static/css/main.7f188edc.chunk.css"
+  },
+  {
+    "revision": "ae4989c3046564355f18",
+    "url": "/Rueli/COMP3104/static/js/main.ae4989c3.chunk.js"
+  },
+  {
+    "revision": "e40723ddbf97f8362d1a",
+    "url": "/Rueli/COMP3104/static/js/runtime~main.e40723dd.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Rueli/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "56ca7ce01e590049c6638ef317589d7a",
+    "url": "/Rueli/COMP3104/index.html"
+  }
+];
